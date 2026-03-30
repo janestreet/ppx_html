d3ed69353b50ab9737c288e7622a020398806fe1
public-release <as-public-release@janestreet.com>
2026-03-30 16:05:18 +0100

    v0.18~preview.130.91+190

--- test/expansion_tests/test_nested_component_syntax.ml
+++ test/expansion_tests/test_nested_component_syntax.ml
@@ -27,3 +27,22 @@ let%expect_test "Nested component as optional arg" =
     Foo.f [] ?arg1:(Html_syntax.Node.div [])
     |}]
 ;;
+
+let%expect_test "Whitespace inside parens in nested argument" =
+  test
+    {|<Foo.f ~arg1:(
+        <div>
+          <div>Hi</div>
+          <div>Hi</div>
+        </div>
+      ) />|};
+  [%expect
+    {|
+    same output between ppx_html and ppx_html_kernel
+
+    Foo.f ()
+      ~arg1:(Html_syntax.Node.div
+               [Html_syntax.Node.div [Html_syntax.Node.Primitives.text "Hi"];
+               Html_syntax.Node.div [Html_syntax.Node.Primitives.text "Hi"]])
+    |}]
+;;
